cbb1fc4b36c805ca50632bb53f6175e2f4ac9eac
Simone Mariotti <campagnolo88@gmail.com>
2013-05-21 01:01:11 +0200

    Corretto il redirect a index.php dopo il logout (Ora funziona anche su Firefox)

--- logout.php
+++ logout.php
@@ -1,23 +1,14 @@
 <?php
 if (isset($_GET['logout']) && $_GET['logout'] === 'true') {
-    
+    header("location:index.php ");
     session_destroy();
-   
-    $port = '';
-    if (isset($_SERVER['SERVER_PORT'])) {
-        $port = ":" . $_SERVER['SERVER_PORT'];
-    }
-    header("location:index.php "); 
-
 }
+
 if (isset($username)) {
     $port = '';
-
     if (isset($_SERVER['SERVER_PORT'])) {
         $port = ":" . $_SERVER['SERVER_PORT'];
     }
-    // echo "<div class=\"logout\">Sei loggato come " . $_SESSION['username'] . ". <a href=\"http://" . $_SERVER["SERVER_NAME"].$port . $_SERVER['PHP_SELF'] . "?logout=true\">(Logout)</a></div>";
-    echo "<div class=\"logout\">Sei loggato come " . $username . ". <a href=\"?logout=true\">(Logout)</a></div>";
-
+    echo "<div class=\"logout\">Sei loggato come " . $username . ". (<a href=\"?logout=true\">Logout</a>)</div>";
 }
 ?>
