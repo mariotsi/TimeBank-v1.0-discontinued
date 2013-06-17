5732fbccdc2e134d30af39760e903017f2dc6404
Simone Mariotti <campagnolo88@gmail.com>
2013-06-17 16:00:35 +0200

    Il link "Registrati" non viene visualizzato se l'utente è loggato, ad eccezione della Home Page

--- menu.php
+++ menu.php
@@ -1,5 +1,3 @@
-<?php
-?>
 <ul id="menu">
     <li>
         <a href="index.php" <?php if (strpos($_SERVER['PHP_SELF'], 'index.php') !== false) echo "style=\"font-weight: bold; color: #B4BA22; font-style: italic\"" ?>>Ricerca
@@ -7,7 +5,8 @@
     <li>
         <a href="annuncio.php"  <?php if (strpos($_SERVER['PHP_SELF'], 'annuncio.php') !== false && strpos($_SERVER['PHP_SELF'], 'aannuncio.php') == false) echo "style=\"font-weight: bold; color: #B4BA22; font-style: italic\"" ?>>Inserisci
             Annuncio</a></li>
-    <li>
+    <?php if (!isset($_SESSION)) { ?>
+        <li>
         <a href="registrazione.php"  <?php if (strpos($_SERVER['PHP_SELF'], 'registrazione.php') !== false) echo "style=\"font-weight: bold; color: #B4BA22; font-style: italic\"" ?>>Registrati</a>
-    </li>
+        </li>                         <?php } ?>
 </ul>
